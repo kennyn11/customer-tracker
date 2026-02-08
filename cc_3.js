9821105640c1f4477691bd39d07bdc07b6749394
Kenny Nguyen <kennyn@usf.edu>
2026-02-08 18:04:32 -0500

    commit changes to js file

---
+++ cc_3.js
@@ -0,0 +1,25 @@
+let customers = [
+ {
+    name: "Kentrell Gaulden"
+    email: "4ktofficial@gmail.com"
+    purchases: "Headphones", "Microphone", "Laptop"
+
+ },
+ {
+    name: "Kevin Durant"
+    email: "easymoneysniper@outlook.com"
+    purchases: "Basketball","Net", "Shirt"
+
+ },
+ {
+    name: "Tim Cheese"
+    email: "timthecheesiest@yahoo.com"
+    purchases: "Cheese", "Gloves", "Plastic Bowls"
+ }
+]
+customers.push({
+    name: "John Pork"
+    email: "porkyjohn@gmail.com"
+    purchases: "Napkins", "Plates", "Forks"
+})
+customers.shift
